ecbacb9a9dc859ca41826bb53726f8ddb818c93c
Nate Miller <nmiller2379@gmail.com>
2024-08-14 12:48:52 -0400

    basic recipe detail rendering

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import RecipeDetail from "./pages/RecipeDetail/RecipeDetail";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -14,6 +14,7 @@ function App() {
         </Routes>
       </Router>
     </div>
+    
   );
 }
 
--- src/pages/RecipeDetail/RecipeDetail.js
+++ src/pages/RecipeDetail/RecipeDetail.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Header from "../../components/Header/Header";
+import "./RecipeDetail.css";
 
 export default function RecipeDetail() {
   const { id } = useParams();
-  const [recipe, setRecipe] = useState(null);
+  const [recipe, setRecipe] = useState({});
 
   useEffect(() => {
     console.log(`Fetching recipe with id: ${id}`);
@@ -13,12 +14,41 @@ export default function RecipeDetail() {
       console.log(response.data);
       setRecipe(response.data);
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  const { mealType, ingredients, instructions } = recipe;
+  const renderMealType = () => {
+    if (mealType) {
+      return mealType.map((type, index) => {
+        if (index === 0) {
+          return <span key={index}>{type}</span>;
+        }
+        return <span key={index}> | {type}</span>;
+      });
+    }
+  };
   return (
     <div>
-      <Header title={recipe?.name} />
-      <p>{recipe?.description}</p>
-      <img src={recipe?.image} alt={recipe?.name} style={{width: "25%"}} />
+      <Header title={recipe.name} />
+      <div>
+        <strong>Best for: </strong>
+        {renderMealType()}
+      </div>
+      <img src={recipe.image} alt={recipe.name} style={{ width: "25%" }} />
+      <h2>Ingredients</h2>
+      <ol>
+        {ingredients &&
+          ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+      </ol>
+      <h2>Instructions</h2>
+      <ol>
+        {instructions &&
+          instructions.map((instruction, index) => (
+            <li key={index}>{instruction}</li>
+          ))}
+      </ol>
     </div>
   );
 }
